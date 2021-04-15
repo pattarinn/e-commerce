e35547b671244321f7938701f8f237ee82cd5552
Pattarin <pattarin.won@ku.th>
2021-04-15 23:32:24 +0700

    fix bug for cover image

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   #   resources :categories
   # end
 
-  resources :products
+  resources :products do
+    member do
+      delete 'remove_image_attachment'
+    end
+  end
   resources :categories
   root 'products#index'
   get 'home/index'
@@ -14,6 +18,5 @@ Rails.application.routes.draw do
 
   namespace :products do
     post 'csv_upload'
-    put 'remove_image_attachment'
   end
 end
