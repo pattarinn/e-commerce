5ee4775a1116a79fad1e6a97085d96265756260d
Pattarin <pattarin.won@ku.th>
2021-04-22 01:26:32 +0700

    edit db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "e_commerce_production"
+  # config.active_job.queue_name_prefix = "blog_production"
 
   config.action_mailer.perform_caching = false
 
@@ -117,6 +117,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
